a0c60271628c8f6a5981546297e2f2f76d6d0ce7
ZandarB <BEKEZA1@student.op.ac.nz>
2025-03-28 10:48:04 +1300

    begun attempting to fix widget

--- src/lib/AlertsRSS.svelte
+++ src/lib/AlertsRSS.svelte
@@ -1,4 +1,5 @@
 <script>
+  
 </script>
 
 <section>
@@ -7,6 +8,7 @@
     <div id="rsswidget" style="height: 500px;">
       <iframe
         src="https://www.rssfeedwidget.com/getrss.php?time=1730414224022&amp;x=https%3A%2F%2Falerts.metservice.com%2Fcap%2Frss&amp;w=200&amp;h=500&amp;bc=333333&amp;bw=1&amp;bgc=transparent&amp;m=20&amp;it=false&amp;t=(default)&amp;tc=333333&amp;ts=15&amp;tb=transparent&amp;il=true&amp;lc=000000&amp;ls=14&amp;lb=true&amp;id=true&amp;dc=333333&amp;ds=14&amp;idt=true&amp;dtc=284F2D&amp;dts=12"
+        
         border="0"
         frameborder="no"
         style="border: 0; padding: 0; margin: 0; width: 340px; height: 500px;"
